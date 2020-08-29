7fab0f71817211a1b8f6439454231208d9a5bfad
JakubK <jakubkuniszewski1@gmail.com>
2020-08-29 21:20:26 +0200

    Install Automapper and Configure it in Startup

--- CardGamesAPI/Startup.cs
+++ CardGamesAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CardGamesAPI.Data;
 using CardGamesAPI.Repositories;
 using HashidsNet;
@@ -28,8 +29,9 @@ namespace CardGamesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
             services.AddTransient<IHashids>(hashids => new Hashids("randomSalt",7));
-            services.AddSingleton<ILiteDbContext,LiteDbContext>();
+            services.AddSingleton<ILiteDbContext>(ctx => new LiteDbContext("Filename=database.db"));
             services.AddSingleton<IDeckRepository,DeckRepository>();
             services.AddControllers();
             services.AddSwaggerGen();
