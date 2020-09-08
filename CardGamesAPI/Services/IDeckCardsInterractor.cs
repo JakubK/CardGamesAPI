5e05976169d858dde6e2ebb57c7eb47551d9750f
JakubK <jakubkuniszewski1@gmail.com>
2020-09-08 20:33:18 +0200

    Create interface for IDeckCardsInterractor

---
+++ CardGamesAPI/Services/IDeckCardsInterractor.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using CardGamesAPI.Models;
+
+namespace CardGamesAPI.Services
+{
+    public interface IDeckCardsInterractor
+    {   
+        List<Card> Draw(CollectionDirection direction, string hash, int count);
+        void Insert(string hash, CollectionDirection direction, Card card);
+        void Shuffle(string hash);
+    }
+}
